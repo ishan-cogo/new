5d1471b8b067046df7a0a1d8ecb2caf190589fef
ISHAN JAIN <ishan.jain@cogoport.com>
2023-02-03 00:20:57 +0530

    bubble_done

---
+++ bubble_sort.rb
@@ -0,0 +1,20 @@
+def fun(arr)
+  n=arr.length()
+  for i in 0..n-1
+    j=i
+    for k in i..n-1
+      if(arr[k]<arr[j])
+        j=k
+      end
+    end
+    temp=arr[j]
+    arr[j]=arr[i]
+    arr[i]=temp
+  end
+  for i in 0..n-1
+    puts(arr[i])
+  end
+end
+
+# arr=[2,4,2,1,1,5,2,3,4]
+# fun(arr)
